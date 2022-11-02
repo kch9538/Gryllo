3723b4cabf13b5228900a4f6912a6be330b91c66
samkim <ch9538@naver.com>
2022-11-02 13:51:35 +0900

    회원가입(이메일인증)구현

--- src/main/java/com/project/gryllo/member/model/MemberInput.java
+++ src/main/java/com/project/gryllo/member/model/MemberInput.java
@@ -5,6 +5,7 @@ import lombok.ToString;
 
 
 
+
 @ToString
 @Data
 public class MemberInput {
--- src/main/java/com/project/gryllo/member/service/MemberService.java
+++ src/main/java/com/project/gryllo/member/service/MemberService.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 
 
+
 public interface MemberService extends UserDetailsService {
 
 	boolean register(MemberInput parameter);
