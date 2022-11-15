51cf5f97b3d086aeec95f6a32842282d1483c3b9
samkim <ch9538@naver.com>
2022-11-16 01:58:45 +0900

    일전 작업내용 삭제

--- src/main/java/com/project/gryllo/controller/exception/InvalidMemberException.java
+++
@@ -1,7 +0,0 @@
-package com.project.gryllo.controller.exception;
-
-public class InvalidMemberException extends RuntimeException {
-    public InvalidMemberException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/project/gryllo/service/rule/FileNaming.java
+++
@@ -1,7 +0,0 @@
-package com.project.gryllo.service.rule;
-
-public interface FileNaming {
-	String makeUniquePrefix(String originalFilename);
-
-	String makePath();
-}
